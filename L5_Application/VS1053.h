6bef14a26df0e719d362b3c23155b8c8337da3e8
jtumath <tumathjohn@gmail.com>
2018-11-22 01:33:06 -0800

    Added somewhat working mp3 class

---
+++ L5_Application/VS1053.h
@@ -0,0 +1,45 @@
+/*
+ * VS1053.h
+ *
+ *  Created on: Nov 21, 2018
+ *      Author: John (Offline)
+ */
+
+#ifndef VS1053_H_
+#define VS1053_H_
+
+#include "vs10xx_uc.h"
+#include "LabSPI0.h"
+#include "gpio.hpp"
+#include <FreeRTOS.h>
+#include "task.h"
+//#include "ssp0.h"
+#include "storage.hpp"
+#include <stdio.h>
+
+
+class VS1053 {
+public:
+    void init(LPC1758_GPIO_Type DREQ, LPC1758_GPIO_Type _CS, LPC1758_GPIO_Type _DCS);
+    void spiwrite(uint8_t c);
+    void spiwrite(uint8_t *c, uint16_t num);
+    uint8_t spiread(void);
+    void sciWrite(uint8_t addr, uint16_t data);
+    uint16_t sciRead(uint8_t addr);
+    void sineTest(uint8_t n, uint16_t ms);
+    void soft_reset(void);
+    void setVolume(uint8_t left, uint8_t right);
+    bool startPlayMP3File(const char *track);
+    virtual ~VS1053();
+    VS1053();
+private:
+    GPIO* DREQ;
+    GPIO* _CS;
+    GPIO* _DCS;
+    LabSpi0 SPI0;
+    FIL currentTrack;
+    bool playingMusic;
+    uint8_t musicBuffer[512];
+};
+
+#endif /* VS1053_H_ */
--- L5_Application/main.cpp
+++ L5_Application/main.cpp
@@ -3,38 +3,26 @@
 #include <stdint.h>
 #include <FreeRTOS.h>
 #include <stdio.h>
-#include "VS1053B.h"
+#include "VS1053.h"
 
-VS1053B* mp3 = VS1053B::getInstance();
+VS1053 MP3;
 
-void print_mp3_status(void)
+void SineTest(void * pvParameters)
 {
-    VS1053b_status_t mp3status;
-    mp3status.status = mp3->ReadSci(0x01);
-    printf("STATUS: %x\n", mp3status.status);
-    printf("SS_DO_NOT_JUMP: %d\n", mp3status.SSREFERENCESEL);
-    printf("SSADCLOCK: %d\n", mp3status.SSADCLOCK);
-    printf("SSAPDOWN1: %d\n", mp3status.SSAPDOWN1);
-    printf("SSAPDOWN2: %d\n", mp3status.SSAPDOWN2);
-    printf("SSVER: %d\n", mp3status.SSVER);
-    printf("SSVCMDISABLE: %d\n", mp3status.SSVCMDISABLE);
-    printf("SSVCMOVERLOAD: %d\n", mp3status.SSVCMOVERLOAD);
-    printf("SSSWING: %d\n", mp3status.SSSWING);
-    printf("SSDONOTJUMP: %d\n", mp3status.SSDONOTJUMP);
-}
+    MP3.init(P1_28, P1_29, P1_23);
+    while(1)
+    {
+        MP3.startPlayMP3File("1:Test002.mp3");
+    }
 
-void PrintStatus(void * pvParameters)
-{
-    print_mp3_status();
-    vTaskDelay(2000);
 }
 
 int main(void)
 {
-    mp3->init_VS1053B();
+    MP3.init(P1_28, P1_29, P1_23);
     const uint32_t STACK_SIZE_WORDS = 2048;
-    xTaskCreate(PrintStatus, "MP3_Status", STACK_SIZE_WORDS, NULL, 1, NULL);
-    scheduler_add_task(new terminalTask(PRIORITY_HIGH));
+    xTaskCreate(SineTest, "SineTest", STACK_SIZE_WORDS, NULL, PRIORITY_HIGH, NULL);
+    scheduler_add_task(new terminalTask(2));
     scheduler_start();
     return -1;
 }
